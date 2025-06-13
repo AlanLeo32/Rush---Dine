2d22bb3d1a1f8e1eda1ec855f5596f94658e3026
NicoAgHerrera <132090401+NicoAgHerrera@users.noreply.github.com>
2025-06-13 16:18:02 -0300

    Corrección menu cocinar

--- rush-n-dine/disponible_item.gd
+++ rush-n-dine/disponible_item.gd
@@ -1,25 +1,30 @@
-extends Button
+extends Control
 
 signal seleccionado(id)
 
 var disp_id
+var start_pos := Vector2.ZERO
+var moved := false
+var threshold := 10
 
-func set_data(id, receta_data,cantidad):
+func set_data(id, receta_data, cantidad):
 	disp_id = id
-	print("Actualizando item: ", id, " cantidad: ", cantidad)  # <--- Agrega este print
-	# Imagen principal
+	print("Actualizando item: ", id, " cantidad: ", cantidad)
+
 	$CanvasLayer/ImagenReceta.texture = receta_data["imagen"]
-
-	# Nombre
 	$CanvasLayer/Nombre.text = receta_data["nombre"]
-
-	# Precio
 	$CanvasLayer/Precio.text = str(receta_data["precio"])
-
-	# Popularidad
 	$CanvasLayer/Popularidad.text = str(receta_data["popularidad"])
-
 	$CanvasLayer/Cantidad.text = str(cantidad)
 
-func _pressed():
-	emit_signal("seleccionado", disp_id)
+func _gui_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			start_pos = event.position
+			moved = false
+		else:
+			if not moved:
+				emit_signal("seleccionado", disp_id)
+	elif event is InputEventMouseMotion:
+		if (event.position - start_pos).length() > threshold:
+			moved = true
